f2295f5a3a68cf0052cf52c6403add28d7da027b
Matthew Hilton <matthewhilton@catalyst-au.net>
2022-10-20 10:16:01 +1000

    ci: fix version matrix

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2022041908;                 // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041904;                 // Requires this Moodle version.
 $plugin->component = 'local_headlessquiz';       // Full name of the plugin (used for diagnostics).
-$plugin->supported = [400];
+$plugin->supported = [40, 41];
